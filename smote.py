7bfef720aa90fdb5575a32f5524e24f50a99d5f9
saafda <85120441+saafda@users.noreply.github.com>
2025-05-19 10:34:56 +0200

    Add files via upload

--- smote.py
+++ smote.py
@@ -7,11 +7,33 @@ import cv2
 from numpy.typing import NDArray
 
 class SMOTE:
+    """
+    A class that implements Synthesized Minority Oversampling Technique
+
+    Attributes:
+        data (NDArray): data points to sample from.
+    
+    Methods:
+        generate_data(): Generates data points to be used for SMOTE.
+        convex_combine(): Main part of the SMOTE method.
+        smote(): Uses generate_data() and convex_combine() to execute the SMOTE method.
+        plot(): Help method for plotting.
+    """
     
     def __init__(self, data_samples):
         self.data_samples = data_samples
         
     def generate_data(self, ratio:float, N:int) -> tuple[NDArray, NDArray]:
+        """Generates N classification data points for two classes with given class ratio
+
+        Args:
+            ratio (float): Ratio of classes, of the form class_0:class_1.
+                           Class 1 is the one to be classified. 
+            N (int): Number of data points to be generated.
+
+        Returns:
+            tuple[NDArray, NDArray]: tuple containing class 0 and class 1 data points
+        """
         class_0 = self.data_samples[self.data_samples[:, 2] == 0]
         class_1 = self.data_samples[self.data_samples[:, 2] == 1]
         N0 = int((1-ratio)*N)
@@ -23,7 +45,17 @@ class SMOTE:
         return class_0_sampled, class_1_sampled
         
     
-    def convex_combine(self, minority_class, k):
+    def convex_combine(self, minority_class: NDArray, k: int) -> NDArray:
+        """Generates a convex combination between a randomly selected point in class 1
+           and one of its random neighbours (also in class 1).
+
+        Args:
+            minority_class (NDArray): all data points of class 1.
+            k (int): number of neighbours we would like to consider in kNN.
+
+        Returns:
+            NDArray: synthesized data point on the form (x, y, 1).
+        """
         random_data_point = minority_class[rd.randint(0,len(minority_class))]
         all_neighbours = minority_class[:,:2]
 
@@ -41,12 +73,31 @@ class SMOTE:
         return convex_combination
 
     def smote(self, minority_class: NDArray, M: int, k: int) -> NDArray:
+        """Uses SMOTE to add M data points of class 1 to the given data points of class 1 (minority_class).
+
+        Args:
+            minority_class (NDArray): all data points of class 1.
+            M (int): Number of data points to be synthesized.
+            k (int): number of neighbours we would like to consider in kNN.
+
+        Returns:
+            NDArray: all class 1 data points in addition to the M new synthesized data points.
+        """
         for i in range(M):
             synthesized_point = self.convex_combine(minority_class, k)
             minority_class = np.vstack([minority_class, synthesized_point])
         return minority_class
     
-    def plot(self, class_0, class_1, image, cmap, size):
+    def plot(self, class_0: NDArray, class_1: NDArray, image: NDArray, cmap: str, size: tuple[int, int]) -> None:
+        """Plots class 0, class 1 and true class boundaries.
+
+        Args:
+            class_0 (NDArray): all data points of class 0.
+            class_1 (NDArray): all data points of class 1.
+            image (NDArray): true class boundaries.
+            cmap (str): color map to use.
+            size (int): figure size.
+        """
         c00, c01 = class_0
         c10, c11 = class_1
         fig, ax = plt.subplots(1, 2)
@@ -63,6 +114,13 @@ class SMOTE:
         plt.show()
         
 class pi():
+    """
+    A simple class for generating data given by class boundaries from a pi symbol.
+
+    Attributes:
+        data (NDArray): data points to sample from.
+        image (NDArray): true class boundaries.
+    """
     def __init__(self):
         im_frame = Image.open('pi3.jpg')
         image = np.array(im_frame.getdata())
@@ -80,6 +138,13 @@ class pi():
         self.image = image
 
 class circles():
+    """
+    A simple class for generating data given by class boundaries from two balls in a 2d plane.
+
+    Attributes:
+        data (NDArray): data points to sample from.
+        image (NDArray): true class boundaries.
+    """
     def __init__(self):
         size = 256
         image = np.zeros((size, size), dtype=np.uint8)
